ae17d6e52ac08315bbabc922af888fb4373fdd64
IsaAfonsoCollares <isabela_afonso_collares@hotmail.com>
2022-06-21 17:20:25 -0300

    Day 9

--- Day7_HangMan/Readme.md
+++ Day7_HangMan/Readme.md
@@ -2,7 +2,7 @@ English
 Project made for the 100 Days of Code: The Complete Python Pro Bootcamp for 2022 course  
 Purpose: Create a Hangman game   
 Development:  
-In this project I used basic python language commands such as print, input, creating lists, defining variables, if's and for and while loops, in addition to the random package to randomize the choice of word and definition of functions.  
+In this project I used basic python language commands such as print, input, creating lists, defining variables, if's, and for and while loops, in addition to the random package to randomize the choice of word and definition of functions.  
 To increase the difficulty level, a while loop was used so that the user could decide to run the program again without having to restart it, two if's to choose the game's language and package imports to access the logo arts and the design of the force at different stages.  
   
 Português  
@@ -10,4 +10,4 @@ Projeto feito para o curso 100 Days of Code: The Complete Python Pro Bootcamp fo
 Objetivo: Criar um jogo da Forca  
 Desenvolvimento:  
 Neste projeto utilizei comandos básicos da linguagem python como print, input, criação de listas,definição de variáveis, if's e loops de for e while, ​​além do pacote random para randomizar a escolha da palavra e definição de funções.  
-Para aumentar o nível de dificuldade, foi usado um loop while para que o usuário pudesse decidir executar o programa novamente sem precisar reiniciá-lo, if para escolha da lingua do jogo, importações de pacotes para acessar as artes do logo e o desonho da forca em diferentes estágios.   
+Para aumentar o nível de dificuldade, foi usado um loop while para que o usuário pudesse decidir executar o programa novamente sem precisar reiniciá-lo, if para escolha da lingua do jogo, importações de pacotes para acessar as artes do logo e o desenho da forca em diferentes estágios.   
--- Day8_Ceasar_Cipher/Ceaser_cipher.py
+++ Day8_Ceasar_Cipher/Ceaser_cipher.py
@@ -1,34 +1,43 @@
-def encode(alphabet, text, shift):
-    final_message=[]
-    message=list(text)
-    for letter in message:
-        n=alphabet.index(letter)
-        if n+shift<=26:
-            new_letter=alphabet[n+shift]
-            final_message.append(new_letter)
-        else:
-            new_letter=alphabet[26-shift]
-            final_message.append(new_letter)
-    return final_message
-def decode(alphabet, text, shift):
-    final_message=[]
-    message=list(text)
-    for letter in message:
-        n=alphabet.index(letter)
-        if n-shift>=0:
-            new_letter=alphabet[n-shift]
-            final_message.append(new_letter)
-        else:
-            new_letter=alphabet[-shift]
-            final_message.append(new_letter)
-    return final_message
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-if direction == 'encode':
-    final_text=encode(alphabet, text, shift)
-elif direction == 'decode':
-    final_text=decode(alphabet, text, shift)
-print(''.join(map(str, final_text)))
+def ceasar( inicial_text, shift_num, direction_cipher):
+    final_message=""
+    for letter in inicial_text:
+        if letter in alphabet:
+            n=alphabet.index(letter)
+            shift = shift_num%26
+            if direction_cipher == "encode":
+                if n+shift<26:
+                    final_message += alphabet[n+shift]
+                else:
+                    final_message +=alphabet[n+shift-25]
+            elif direction_cipher =="decode":
+                if n-shift>=0:
+                    final_message += alphabet[n-shift]
+                else:
+                    final_message +=alphabet[n-shift+25]
+        else:
+            final_message += letter
+                
+    print(final_message)
+should_end = False
+language=int(input("Welcome to the Ceaser Cipher! What languafe would you like  to use?\n1-English\n2-Português\n"))
+if language ==1:
+    while not should_end:
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        ceasar(text, shift, direction)
+        restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+        if restart == "no":
+            should_end = True
+            print("Goodbye")
+elif language==2:
+    while not should_end:
+        direction = input("Digite encode para 'codificar' e decode para 'decodificar':\n")
+        text = input("Digite sua mensagem:\n").lower()
+        shift = int(input("Digite o número de posições:\n"))
+        ceasar(text, shift, direction)
+        restart = input("Digite yes para utilizar o programa novamente ou no para encerrá-lo.\n")
+        if restart == "no":
+            should_end = True
+            print("Adeus")
