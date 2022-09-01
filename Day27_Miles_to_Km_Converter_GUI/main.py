ab825130128a771e5736f1ed42c74ebfa3bafe51
IsaAfonsoCollares <isabela_afonso_collares@hotmail.com>
2022-09-01 18:08:46 -0300

    Day 27

--- Day27_Miles_to_Km_Converter_GUI/main.py
+++ Day27_Miles_to_Km_Converter_GUI/main.py
@@ -1,25 +1,33 @@
 import tkinter as tk
-FONT = ("Arial", 24, "bold")
+
+FONT = ("Arial", 14, "bold")
+
+
+def miles_km_converter():
+    miles = miles_input.get()
+    km["text"] = int(miles) * 1.6093
+
+
 window = tk.Tk()
 window.title("Miles to Km Converter")
-window.minsize(width=500, height=300)
-my_label = tk.Label(text="Converter", font=FONT)
-my_label.pack()
-my_label["text"] = "New York"
+window.minsize(width=100, height=100)
+window.config(padx=40, pady=20)
+
+text_miles = tk.Label(text="miles", font=FONT)
+text = tk.Label(text="is equal to ", font=FONT)
+text_km = tk.Label(text="km", font=FONT)
+miles_input = tk.Entry(width=10)
+km = tk.Label(text="0", font=FONT)
+
+text_miles.grid(column=1, row=0)
+text.grid(column=2, row=0)
+text_km.grid(column=4, row=0)
+miles_input.grid(column=0, row=0)
+km.grid(column=3, row=0)
 
 
-
-
-new = tk.Entry(width=10)
-new.pack()
-
-def button_click():
-    new_text = new.get()
-    my_label.config(text=new_text)
-
-
-button = tk.Button(text="Click me", command=button_click)
-button.pack()
+button = tk.Button(text="Calculate", command=miles_km_converter)
+button.grid(column=2, row=1)
 
 
 window.mainloop()
